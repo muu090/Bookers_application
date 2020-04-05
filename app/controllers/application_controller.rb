0c281363fb388be11d67c784a17038b15031eaf6
Sota <sooota09@gmail.com>
2020-04-06 02:23:58 +0900

    [Fix] deviseのパラメータ許可追加(name,email)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-  # before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email]) # 新規登録時(sign_up時)にname,emailというキーのパラメーターを追加で許可する
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name]) # ログイン時(sign_in時)にnameというキーのパラメーターを追加で許可する
   end
 
 end
